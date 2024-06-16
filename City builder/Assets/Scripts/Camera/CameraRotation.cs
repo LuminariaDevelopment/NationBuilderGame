2ab316f13f3cbac10839ef67ce6a5d54dc59376c
Luminary-Interactive <zhengyuanma111@gmail.com>
2024-06-16 05:50:46 -0500

    camera new
    camera system new

--- City builder/Assets/Scripts/Camera/CameraRotation.cs
+++ City builder/Assets/Scripts/Camera/CameraRotation.cs
@@ -1,31 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraRotation : MonoBehaviour
-{
-    float horizontalRotation;
-    public float sensetivityVisual;
-    public static float sensitivity;
+namespace CameraControl {
+	public class CameraRotation : MonoBehaviour {
+		[SerializeField] private float _speed = 15f;
+		[SerializeField] private float _smoothing = 5f;
+		
+		private float _targetAngle;
+		private float _currentAngle;
+		
+		private void Awake() {
+			_targetAngle = transform.eulerAngles.y;
+			_currentAngle = _targetAngle;
+		}
 
-    private void Start()
-    {
-        sensitivity = sensetivityVisual;
-    }
+		private void HandleInput() {
+			if (!Input.GetMouseButton(1)) return;
+			_targetAngle += Input.GetAxisRaw("Mouse X") * _speed;
+		}
 
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.Mouse2))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            horizontalRotation += Input.GetAxis("Mouse X") * sensitivity;
-            transform.localEulerAngles = new Vector3(0, horizontalRotation, 0);
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-        }
-    }
+		private void Rotate() {
+			_currentAngle = Mathf.Lerp(_currentAngle, _targetAngle, Time.deltaTime * _smoothing);
+			transform.rotation = Quaternion.AngleAxis(_currentAngle, Vector3.up);
+		}
+	
+		private void Update() {
+			HandleInput();
+			Rotate();
+		}
+	}
 }
---
+++ City builder/Assets/Scripts/Camera/CameraZoom.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace CameraControl {
+    public class CameraZoom : MonoBehaviour {
+        [SerializeField] private float zoomSpeed = 5f;
+        [SerializeField] private float zoomSmoothing = 5f;
+        [SerializeField] private Vector2 zoomRange = new Vector2(5f, 20f);
+        [SerializeField] private Transform cameraTransform;
+
+        private float targetZoom;
+        private float currentZoom;
+
+        private void Awake() {
+            currentZoom = targetZoom = -zoomRange.y;
+        }
+
+        private void Update() {
+            HandleInput();
+            Zoom();
+        }
+
+        private void HandleInput() {
+            float zoomInput = Input.GetAxis("Mouse ScrollWheel");
+            targetZoom = Mathf.Clamp(targetZoom + zoomInput * zoomSpeed, -zoomRange.y, -zoomRange.x);
+        }
+
+        private void Zoom() {
+            currentZoom = Mathf.Lerp(currentZoom, targetZoom, Time.deltaTime * zoomSmoothing);
+            Vector3 newPosition = cameraTransform.localPosition;
+            newPosition.z = currentZoom;
+            cameraTransform.localPosition = newPosition;
+        }
+    }
+}
