b08e6684e7f23796a998dd18797d3c57f90453ec
AlvinoP <102621060+Alvinopingvino@users.noreply.github.com>
2024-06-26 19:46:45 +0200

    Finnished CameraCollission

--- City builder/Assets/Scripts/Camera/CameraCollision.cs
+++ City builder/Assets/Scripts/Camera/CameraCollision.cs
@@ -4,16 +4,15 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CameraCollision : MonoBehaviour
 {
-    [SerializeField]int _rayDist;
-
     Transform _cameraTransform;
     CameraMotion _camMotion;
 
-    public LayerMask _layerMask;
-    public LayerMask _inverseLayermask;
+    [SerializeField] LayerMask _layerMask;
+    [SerializeField] LayerMask _inverseLayerMask;
 
     private void Start()
     {
@@ -23,16 +22,20 @@ public class CameraCollision : MonoBehaviour
 
     private void Update()
     {
+        CalculateHight();
+    }
+
+    void CalculateHight()
+    {
         RaycastHit hit;
 
-        if (Physics.Raycast(new Vector3(_cameraTransform.position.x, _cameraTransform.transform.position.y + 10000, _cameraTransform.position.z), Vector3.down, out hit, Mathf.Infinity, _inverseLayermask))
+        if (Physics.Raycast(new Vector3(_cameraTransform.position.x, _cameraTransform.transform.position.y + 10000, _cameraTransform.position.z), Vector3.down, out hit, Mathf.Infinity, _inverseLayerMask))
         {
             _camMotion._targetPosition.y = hit.point.y + 1;
         }
         else if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 10000, transform.position.z), Vector3.down, out hit, Mathf.Infinity, _layerMask))
         {
-                _camMotion._targetPosition.y = hit.point.y + 1;
+            _camMotion._targetPosition.y = hit.point.y + 1;
         }
     }
-
 }
